86ad7bc1e864bcff3b1d4629646cd86b116251f6
sethbunke <sjb_strat@hotmail.com>
2019-11-05 12:34:21 -0800

    sample id server

---
+++ SampleIdServer/TeamX.Security.Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TeamX.Security.Client
+{
+    class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            // discover endpoints from the metadata by calling Auth server hosted on 5000 port
+            var discoveryClient = await DiscoveryClient.GetAsync("http://localhost:5000");
+            if (discoveryClient.IsError)
+            {
+                Console.WriteLine(discoveryClient.Error);
+                return;
+            }
+
+            // request the token from the Auth server
+            var tokenClient = new TokenClient(discoveryClient.TokenEndpoint, "client", "secret");
+            var response = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (response.IsError)
+            {
+                Console.WriteLine(response.Error);
+                return;
+            }
+
+            Console.WriteLine(response.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(response.AccessToken);
+
+            var apiResponse = await client.GetAsync("http://localhost:5001/identity");
+            if (!apiResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine(apiResponse.StatusCode);
+            }
+            else
+            {
+                var content = await apiResponse.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}

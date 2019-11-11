926d80488fb3d00b1d485d59cca60145c5a35665
Seth Bunke <Seth.Bunke@blackline.com>
2019-11-11 22:40:03 +0000

    added no auth controller

---
+++ IdentityServer-client/TeamX.Security.AuthenticationServer-IdentityServerv4--master/TeamX.Security.API/Controllers/IdentityNoAuthController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeamX.Security.API.Controllers
+{
+    [Route("[controller]")]
+    public class IdentityNoAuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var headers = Request.Headers.ToList();
+            var claims = from c in User.Claims select new { c.Type, c.Value };
+            //var output = new { headers, claims };
+            return new JsonResult(new { headers, claims, dt = DateTime.Now.ToLongTimeString() });
+        }
+    }
+}
